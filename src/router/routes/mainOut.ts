efebacad3678bec39d2eb558567f726702650973
wnagxiaomeng <15480513+wnagxiaomeng@user.noreply.gitee.com>
2025-04-19 23:19:28 +0800

    feat: 解决问题

--- src/router/routes/mainOut.ts
+++ src/router/routes/mainOut.ts
@@ -18,15 +18,6 @@ export const mainOutRoutes: AppRouteModule[] = [
     },
   },
   {
-    path: '/main',
-    name: 'main',
-    component: () => import('/@/views/main/index.vue'),
-    meta: {
-      title: '城北街道述求管理平台',
-      ignoreAuth: true,
-    },
-  },
-  {
     path: '/runtime',
     name: 'runtime',
     component: () => import('/@/views/runtime/index.vue'),
@@ -44,15 +35,6 @@ export const mainOutRoutes: AppRouteModule[] = [
       ignoreAuth: true,
     },
   },
-  //   {
-  //     path: '/test',
-  //     name: 'test',
-  //     component: () => imp../../views/main/index.vuet.vue'),
-  //     meta: {
-  //       title: '城北街道述求管理平台',
-  //       ignoreAuth: true,
-  //     },
-  //   },
 ];
 
 export const mainOutRouteNames = mainOutRoutes.map((item) => item.name);
