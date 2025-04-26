d95025f43558ef01b9faf6e1de2519ac1396169b
wnagxiaomeng <15480513+wnagxiaomeng@user.noreply.gitee.com>
2025-04-27 00:36:17 +0800

    feat: 路由跳转

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -45,8 +45,8 @@
   const navItems = [
     { title: '综合事态', path: '/composite' },
     { title: '运行事态', path: '/runtime' },
-    { title: '案件办理', path: '/' },
-    { title: '案件管理', path: '/' },
+    { title: '案件办理', path: '/complain/bizComplaintTicketList' },
+    { title: '案件管理', path: '/complaint/manager' },
   ];
 
   // 更新时间
