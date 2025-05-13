8272035f96c28072a9c356bbfdef6a15df3fcb8a
abuxiaojiejie <yuanpeipei0217@gmail.com>
2025-05-13 23:39:54 +0800

    重点对象页面

---
+++ src/views/complain/priority-person/PriorityEdit.vue
@@ -0,0 +1,194 @@
+<template>
+  <BasicModal
+    v-bind="$attrs"
+    @register="registerDrawer"
+    :title="getTitle"
+    :width="900"
+    @ok="handleSubmit"
+    :showFooter="showFooter"
+    destroyOnClose
+    :maskClosable="false"
+  >
+    <div class="pl-8">
+      <BasicForm @register="registerForm" />
+    </div>
+  </BasicModal>
+</template>
+  <script lang="ts" setup>
+import { ref, computed, unref, useAttrs } from 'vue';
+import { BasicForm, useForm } from '/@/components/Form/index';
+import { formSchema } from './priority.data';
+import { BasicModal, useModalInner } from '/@/components/Modal';
+
+import { editPriority, addPriority } from './priority.api';
+import { useDrawerAdaptiveWidth } from '/@/hooks/jeecg/useAdaptiveWidth';
+
+// 声明Emits
+const emit = defineEmits(['success', 'register']);
+const attrs = useAttrs();
+const isUpdate = ref(true);
+const rowId = ref('');
+const departOptions = ref([]);
+let isFormDepartUser = false;
+//表单配置
+const [registerForm, { setProps, resetFields, setFieldsValue, validate, updateSchema }] = useForm({
+  labelWidth: 150,
+  schemas: formSchema,
+  showActionButtonGroup: false,
+  layout: 'vertical',
+  rowProps: { gutter: 24, justify: 'center', align: 'middle' },
+  //全局col列占比(每列显示多少位)，和schemas中的colProps属性一致
+  baseColProps: { span: 12, style: { marginBottom: '10px' } },
+  //row行的样式
+  baseRowStyle: { width: '100%' },
+});
+// TODO [VUEN-527] https://www.teambition.com/task/6239beb894b358003fe93626
+const showFooter = ref(true);
+//表单赋值
+const [registerDrawer, { setModalProps, closeModal }] = useModalInner(async (data) => {
+  await resetFields();
+  showFooter.value = data?.showFooter ?? true;
+  setModalProps({ confirmLoading: false });
+  isUpdate.value = !!data?.isUpdate;
+  //   if (unref(isUpdate)) {
+  rowId.value = data.record.id;
+  //     //租户信息定义成数组
+  //     if (data.record.relTenantIds && !Array.isArray(data.record.relTenantIds)) {
+  //       data.record.relTenantIds = data.record.relTenantIds.split(',');
+  //     } else {
+  //       //【issues/I56C5I】用户管理中连续点两次编辑租户配置就丢失了
+  //       //data.record.relTenantIds = [];
+  //     }
+
+  //     //查角色/赋值/try catch 处理，不然编辑有问题
+  //     try {
+  //       const userRoles = await getUserRoles({ userid: data.record.id });
+  //       if (userRoles && userRoles.length > 0) {
+  //         data.record.selectedroles = userRoles;
+  //       }
+  //     } catch (error) {}
+
+  //     //查所属部门/赋值
+  //     const userDepart = await getUserDepartList({ userId: data.record.id });
+  //     if (userDepart && userDepart.length > 0) {
+  //       data.record.selecteddeparts = userDepart;
+  //       let selectDepartKeys = Array.from(userDepart, ({ key }) => key);
+  //       data.record.selecteddeparts = selectDepartKeys.join(',');
+  //       departOptions.value = userDepart.map((item) => {
+  //         return { label: item.title, value: item.key };
+  //       });
+  //     }
+  //     //负责部门/赋值
+  //     data.record.departIds && !Array.isArray(data.record.departIds) && (data.record.departIds = data.record.departIds.split(','));
+  //     //update-begin---author:zyf   Date:20211210  for：避免空值显示异常------------
+  //     //update-begin---author:liusq   Date:20231008  for：[issues/772]避免空值显示异常------------
+  //     data.record.departIds =  (!data.record.departIds || data.record.departIds == '') ? [] : data.record.departIds;
+  //     //update-end-----author:liusq   Date:20231008  for：[issues/772]避免空值显示异常------------
+  //     //update-begin---author:zyf   Date:20211210  for：避免空值显示异常------------
+  //   }
+  //   //处理角色用户列表情况(和角色列表有关系)
+  //   data.selectedroles && (await setFieldsValue({ selectedroles: data.selectedroles }));
+  //   // -update-begin--author:liaozhiyang---date:20240702---for：【TV360X-1737】部门用户编辑接口，增加参数updateFromPage:"deptUsers"
+  //   isFormDepartUser = data?.departDisabled === true ? true : false;
+  //   // -update-end--author:liaozhiyang---date:20240702---for：【TV360X-1737】部门用户编辑接口，增加参数updateFromPage:"deptUsers"
+  //   //编辑时隐藏密码/角色列表隐藏角色信息/我的部门时隐藏所属部门
+  //   updateSchema([
+  //     {
+  //       field: 'password',
+  //       // 【QQYUN-8324】
+  //       ifShow: !unref(isUpdate),
+  //     },
+  //     {
+  //       field: 'confirmPassword',
+  //       ifShow: !unref(isUpdate),
+  //     },
+  //     {
+  //       field: 'selectedroles',
+  //       show: !data.isRole,
+  //     },
+  //     {
+  //       field: 'departIds',
+  //       componentProps: { options: departOptions },
+  //     },
+  //     {
+  //       field: 'selecteddeparts',
+  //       show: !data?.departDisabled,
+  //     },
+  //     {
+  //       field: 'selectedroles',
+  //       show: !data?.departDisabled,
+  //       //update-begin---author:wangshuai ---date:20230424  for：【issues/4844】多租户模式下，新增或编辑用户，选择角色一栏，角色选项没有做租户隔离------------
+  //       //判断是否为多租户模式
+  //       componentProps:{
+  //         api: data.tenantSaas?getAllRolesList:getAllRolesListNoByTenant
+  //       }
+  //       //update-end---author:wangshuai ---date:20230424  for：【issues/4844】多租户模式下，新增或编辑用户，选择角色一栏，角色选项没有做租户隔离------------
+  //     },
+  //     //update-begin---author:wangshuai ---date:20230522  for：【issues/4935】租户用户编辑界面中租户下拉框未过滤，显示当前系统所有的租户------------
+  //     {
+  //       field: 'relTenantIds',
+  //       componentProps:{
+  //         disabled: !!data.tenantSaas,
+  //       },
+  //     },
+  //     //update-end---author:wangshuai ---date:20230522  for：【issues/4935】租户用户编辑界面中租户下拉框未过滤，显示当前系统所有的租户------------
+  //   ]);
+  //   //update-begin---author:wangshuai ---date:20230522  for：【issues/4935】租户用户编辑界面中租户下拉框未过滤，显示当前系统所有的租户------------
+  //   if(!unref(isUpdate) && data.tenantSaas){
+  //     await setFieldsValue({ relTenantIds: getTenantId().toString() })
+  //   }
+  //update-end---author:wangshuai ---date:20230522  for：【issues/4935】租户用户编辑界面中租户下拉框未过滤，显示当前系统所有的租户------------
+  // 无论新增还是编辑，都可以设置表单值
+  if (typeof data.record === 'object') {
+    setFieldsValue({
+      ...data.record,
+    });
+  }
+  // 隐藏底部时禁用整个表单
+  //update-begin-author:taoyan date:2022-5-24 for: VUEN-1117【issue】0523周开源问题
+  setProps({ disabled: !showFooter.value });
+  //update-end-author:taoyan date:2022-5-24 for: VUEN-1117【issue】0523周开源问题
+});
+//获取标题
+const getTitle = computed(() => {
+  // update-begin--author:liaozhiyang---date:20240306---for：【QQYUN-8389】系统用户详情抽屉title更改
+  if (!unref(isUpdate)) {
+    return '新增重点对象';
+  } else {
+    return '编辑重点对象';
+  }
+  // update-end--author:liaozhiyang---date:20240306---for：【QQYUN-8389】系统用户详情抽屉title更改
+});
+const { adaptiveWidth } = useDrawerAdaptiveWidth();
+
+//提交事件
+async function handleSubmit() {
+  try {
+    let values = await validate();
+    setModalProps({ confirmLoading: true });
+    // values.userIdentity === 1 && (values.departIds = '');
+    let isUpdateVal = unref(isUpdate);
+    // values.type = +values.type;
+    // -update-begin--author:liaozhiyang---date:20240702---for：【TV360X-1737】部门用户编辑接口，增加参数updateFromPage:"deptUsers"
+    let params = values;
+    // if (isFormDepartUser) {
+    //   params = { ...params, updateFromPage: 'deptUsers' };
+    // }
+    // -update-end--author:liaozhiyang---date:20240702---for：【TV360X-1737】部门用户编辑接口，增加参数updateFromPage:"deptUsers"
+    //提交表单
+    if (!isUpdateVal) {
+      await addPriority(params);
+    } else {
+      await editPriority({ ...params, id: rowId.value });
+    }
+
+    //关闭弹窗
+    closeModal();
+    //刷新列表
+    emit('success', { isUpdateVal, values });
+  } finally {
+    setModalProps({ confirmLoading: false });
+  }
+}
+</script>
+  
--- src/views/complain/priority-person/index.vue
+++ src/views/complain/priority-person/index.vue
@@ -1,3 +1,158 @@
 <template>
-    <h1>重点对象</h1>
+  <!--引用表格-->
+  <BasicTable @register="registerTable" :rowSelection="rowSelection">
+    <!--插槽:table标题-->
+    <template #tableTitle>
+      <a-button type="primary" preIcon="ant-design:plus-outlined" @click="handleCreate"> 新增</a-button>
+      <!-- <a-button type="primary" preIcon="ant-design:export-outlined" @click="onExportXls" :disabled="isDisabledAuth('system:user:export')"> 导出</a-button> -->
+      <!-- <j-upload-button type="primary" preIcon="ant-design:import-outlined" @click="onImportXls">导入word</j-upload-button> -->
+      <!-- <a-button type="primary" @click="showEdit" preIcon="ant-design:hdd-outlined">编辑</a-button> -->
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <template #overlay>
+          <a-menu>
+            <a-menu-item key="1" @click="batchHandleDelete">
+              <Icon icon="ant-design:delete-outlined"></Icon>
+              批量删除
+            </a-menu-item>
+          </a-menu>
+        </template>
+        <a-button
+          >批量操作
+          <Icon icon="mdi:chevron-down"></Icon>
+        </a-button>
+      </a-dropdown>
+    </template>
+    <!--插槽:表格内容-->
+    <template #bodyCell="{ text, column, record }">
+      <template v-if="column.dataIndex === 'caseNumber'">
+        <a href="javascript:void(0)" @click="showEdit(record)">{{ text }}</a>
+      </template>
+    </template>
+    <!--操作栏-->
+    <template #action="{ record }">
+      <TableAction :actions="getTableAction(record)" />
+    </template>
+  </BasicTable>
+
+  <!--工单编辑-->
+  <PriorityEdit @register="registerModal" @success="handleSuccess" />
 </template>
+<script lang="ts" setup>
+import { BasicTable, TableAction, ActionItem } from '/@/components/Table';
+import { useListPage } from '/@/hooks/system/useListPage';
+import { list, deletePriority, deleteBatchPriority } from './priority.api';
+import { columns, searchFormSchema } from './priority.data';
+import { useModal } from '/@/components/Modal';
+import { useMessage } from '/@/hooks/web/useMessage';
+//@ts-ignore
+import PriorityEdit from './PriorityEdit.vue';
+//注册drawer
+const [registerModal, { openModal }] = useModal();
+const { createConfirm } = useMessage();
+// 列表页面公共参数、方法
+const { prefixCls, tableContext, onExportXls, onImportXls } = useListPage({
+  designScope: 'priority-list',
+  tableProps: {
+    title: '重点对象列表',
+    api: list,
+    columns: columns,
+    size: 'small',
+    formConfig: {
+      // labelWidth: 200,
+      schemas: searchFormSchema,
+    },
+    actionColumn: {
+      width: 120,
+      fixed: 'right',
+    },
+    beforeFetch: (params) => {
+      console.log('params', params);
+      return Object.assign(params, { pageNum: params.pageNo });
+    },
+  },
+  // exportConfig: {
+  //   name: '用户列表',
+  //   url: getExportUrl,
+  // },
+  // importConfig: {
+  //   url: getImportUrl,
+  // },
+});
+
+//@ts-ignore 注册table数据
+const [registerTable, { reload }, { rowSelection, selectedRowKeys }] = tableContext;
+
+function getTableAction(record): ActionItem[] {
+  return [
+    {
+      label: '编辑',
+      onClick: handleEdit.bind(null, { ...record, type: record.type.toString() }),
+      // ifShow: () => hasPermission('system:user:edit'),
+    },
+    {
+      label: '删除',
+      popConfirm: {
+        title: '是否确认删除',
+        confirm: handleDelete.bind(null, record),
+      },
+    },
+  ];
+}
+
+function handleEdit(record: Recordable) {
+  openModal(true, {
+    record,
+    isUpdate: true,
+    showFooter: true,
+  });
+}
+
+function handleCreate() {
+  openModal(true, {
+    isUpdate: false,
+    showFooter: true,
+  });
+}
+
+async function handleDelete(record: Recordable) {
+  try {
+    await deletePriority({ id: '' + record.id });
+    reload();
+  } catch (error) {
+    console.error('删除失败', error);
+  }
+}
+
+function batchHandleDelete() {
+  // deleteUser({ ids: selectedRowKeys }).then(() => {
+  //   reload();
+  // });
+  // 确认对话框
+  createConfirm({
+    title: '是否批量删除选中的数据？',
+    content: '删除后数据将不可恢复',
+    iconType: 'warning',
+    onOk: async () => {
+      try {
+        await deleteBatchPriority({ ids: selectedRowKeys.value.join(',') });
+        reload();
+      } catch (error) {
+        console.error('批量删除失败', error);
+      }
+    },
+  });
+  // confirm();
+  console.log('批量删除', selectedRowKeys.value);
+}
+
+/**
+ * 成功回调
+ */
+function handleSuccess() {
+  reload();
+}
+
+function showEdit(record: Recordable) {
+  handleEdit(record);
+}
+</script>
