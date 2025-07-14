095b2d269ae2234d8bb59ce31b0d097f245428e2
jetcage <cagejet@gmail.com>
2025-07-14 11:46:24 +0800

    更新h5分支代码

---
+++ src/router/routes/mobileRouter.ts
@@ -0,0 +1,29 @@
+import type { AppRouteRecordRaw } from '/@/router/types';
+import { H5_LAYOUT } from '/@/router/constant';
+// 移动端，分派h5路由
+export const AssignRouter: AppRouteRecordRaw = {
+  path: '/h5-assign',
+  name: 'H5Assign',
+  component: H5_LAYOUT,
+  redirect: '/h5-assign/index',
+  meta: {
+    title: 'H5Assign',
+    hideBreadcrumb: true,
+    hideChildrenInMenu: true,
+  },
+  children: [
+    {
+      path: 'index',
+      name: 'H5AssignIndex',
+      component: () => import('/@/views/h5/assign/index.vue'),
+      meta: {
+        title: '工单分配',
+        hideBreadcrumb: true,
+        currentActiveMenu: '/h5-assign',
+      },
+    },
+  ],
+};
+
+// 移动端h5路由列表
+export const mobileH5RoutesList = [AssignRouter];
